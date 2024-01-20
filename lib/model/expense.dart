f25e7ef75b7fc3334e5a9499164a1236f6bc776d
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-01-21 00:01:28 +0600

    added enum of category

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -1,15 +1,24 @@
-
 import 'package:uuid/uuid.dart';
 
-final uuid= Uuid();
-class Expense{
+final uuid = Uuid();
+
+enum Category {
+  food,
+  travel,
+  leisure,
+  work,
+}
+
+class Expense {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
   Expense({
     required this.title,
+    required this.category,
     required this.amount,
     required this.date,
-  }): id= uuid.v4();
+  }) : id = uuid.v4();
 }
