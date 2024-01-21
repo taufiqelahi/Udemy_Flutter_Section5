e6f3e9998a96ea20f2987b6084c1b3839802624a
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-01-21 22:35:32 +0600

    added expense list

---
+++ lib/expense_list.dart
@@ -0,0 +1,22 @@
+import 'package:expense_tracker/model/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseList extends StatelessWidget {
+  final List<Expense> expenses;
+  const ExpenseList({super.key, required this.expenses});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) {
+       final expense = expenses[index];
+        return ListTile(
+          title: Text(expense.title),
+          subtitle: Text(expense.category.toString()),
+          trailing: Text(expense.amount.toString()),
+        );
+      },
+    );
+  }
+}
