c3eaa99fb0ef4077bf8aa041c568235db7281bc4
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-01-27 14:24:10 +0600

    added expanse bucket class

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 var kColorSchema =
     ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+var darkColorSchema =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 5, 99, 125));
 
 void main() {
   runApp(const MyApp());
@@ -14,43 +16,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+          colorScheme: darkColorSchema,
+          brightness: Brightness.dark,
+          appBarTheme: AppBarTheme(
+              backgroundColor: darkColorSchema.primaryContainer,
+              foregroundColor: darkColorSchema.onPrimaryContainer),
+          cardTheme: CardTheme().copyWith(
+            color: darkColorSchema.secondaryContainer,
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: darkColorSchema.primaryContainer,
+            ),
+          ),
+          textTheme: TextTheme().copyWith(
+            titleLarge: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+                color: darkColorSchema.onSecondaryContainer),
+            bodyMedium: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.normal,
+              color: darkColorSchema.onSecondaryContainer,
+            ),
+          )),
       theme: ThemeData().copyWith(
-        colorScheme: kColorSchema,
-        appBarTheme: AppBarTheme(
-          backgroundColor: kColorSchema.primaryContainer,
-          foregroundColor: kColorSchema.onPrimaryContainer
-        ),
-        cardTheme: CardTheme().copyWith(
-          color: kColorSchema.secondaryContainer,
-
-          margin: EdgeInsets.symmetric(horizontal: 16,vertical: 6),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorSchema.primaryContainer,
-
+          colorScheme: kColorSchema,
+          appBarTheme: AppBarTheme(
+              backgroundColor: kColorSchema.primaryContainer,
+              foregroundColor: kColorSchema.onPrimaryContainer),
+          cardTheme: CardTheme().copyWith(
+            color: kColorSchema.secondaryContainer,
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 6),
           ),
-
-        ),
-        textTheme: TextTheme().copyWith(
-          titleLarge: TextStyle(
-            fontSize: 14,
-            fontWeight: FontWeight.normal,
-            color: kColorSchema.onSecondaryContainer
-
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorSchema.primaryContainer,
+            ),
           ),
-
-          bodyMedium: TextStyle(
-            fontSize: 14,
-            fontWeight: FontWeight.normal,
-            color: kColorSchema.onSecondaryContainer,
-
-          ),
-
-
-
-        )
-      ),
+          textTheme: TextTheme().copyWith(
+            titleLarge: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+                color: kColorSchema.onSecondaryContainer),
+            bodyMedium: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.normal,
+              color: kColorSchema.onSecondaryContainer,
+            ),
+          )),
+      themeMode: ThemeMode.system,
       home: const ExpensesScreen(),
     );
   }
--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
@@ -33,5 +34,29 @@ class Expense {
 
   String get formattedDate {
     return DateFormat.yMd().format(date);
+  }
+}
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+      .where((expense) => expense.category == category)
+      .toList();
+
+  final Categories category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount; // sum = sum + expense.amount
+    }
+
+    return sum;
   }
 }
